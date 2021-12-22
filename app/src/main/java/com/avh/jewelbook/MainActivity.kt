ebdf89b6cc9a3b285942a47658af351877bd5e3b
gohelvivek <vivekgohel93027@gmail.com>
2021-12-22 14:56:31 +0530

    First commit

---
+++ app/src/main/java/com/avh/jewelbook/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.avh.jewelbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getSupportActionBar()?.hide()
+        setContentView(R.layout.activity_main)
+    }
+}
