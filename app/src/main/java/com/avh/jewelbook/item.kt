c8015d54e54ddfc2bede448ab25b7130189d5e5c
hs-hiteshsolanki <solankihitesh7058@gmail.com>
2022-01-12 14:58:48 +0530

    new

--- app/src/main/java/com/avh/jewelbook/item.kt
+++ app/src/main/java/com/avh/jewelbook/item.kt
@@ -1,40 +1,26 @@
 package com.avh.jewelbook
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
-
-class home : AppCompatActivity() {
-
+class item : AppCompatActivity() {
 
     lateinit var session: sessionmanager
     lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_item)
 
-
-        session = sessionmanager(applicationContext)
-        var lblname = findViewById<TextView>(R.id.name)
-        var lblmail = findViewById<TextView>(R.id.mail)
-
-        session.chackLogin()
-        var user: HashMap<String, String> = session.getUerDetails()
-        var name: String = user.get(sessionmanager.KEY_NAME)!!
-        var mail: String = user.get(sessionmanager.KEY_EMAIL)!!
-        lblname.setText("Name : " + name)
-        lblmail.setText("Company : " + mail)
-
-
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawlout)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerlayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
@@ -55,7 +41,6 @@ class home : AppCompatActivity() {
 
                 R.id.item -> {
                     val intent = Intent(this, item::class.java)
-                    startActivity(intent)
                 }
 
 
@@ -99,6 +84,12 @@ class home : AppCompatActivity() {
             true
         }
 
+        val addbtn = findViewById<Button>(R.id.aitem)
+
+        addbtn.setOnClickListener(){
+            val intent = Intent(this, additem::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
