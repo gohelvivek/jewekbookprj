d317f4a67092f590de0df86fcc521d38564db91d
atulsorani <atulsorani96@gmail.com>
2022-02-08 16:03:06 +0530

    Contact Import

--- app/src/main/java/com/avh/jewelbook/contactImport.kt
+++ app/src/main/java/com/avh/jewelbook/contactImport.kt
@@ -1,49 +1,45 @@
 package com.avh.jewelbook
 
-import android.content.Intent
-import android.net.Uri
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class contactImport : AppCompatActivity() {
-
-    var reqpdf = 21
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_import)
-        var reqpdf = 21
-        var chbtn = findViewById<Button>(R.id.choosebtn)
-        var upbtn = findViewById<Button>(R.id.uploadbtn)
 
-        chbtn.setOnClickListener() {
-            var choosefile = Intent(Intent.ACTION_GET_CONTENT)
-            choosefile.setType("application/pdf")
-            choosefile = Intent.createChooser(choosefile, "Chhose a File")
-            startActivityForResult(choosefile, reqpdf)
-        }
-
-        upbtn.setOnClickListener(){
-
+        var Incnoupload = findViewById<Button>(R.id.Incnoupload)
+        Incnoupload.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(this,
+                    Array(1) { Manifest.permission.READ_CONTACTS },
+                    111)
+            } else {
+                readContact()
+            }
         }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode.equals(reqpdf) && resultCode.equals(RESULT_OK)) {
-
-
-
-           /* var txtchose = findViewById<TextView>(R.id.txt_filechose)
-            txtchose.setText("Document Selected")
-            Toast.makeText(applicationContext, "Document Selected", Toast.LENGTH_SHORT).show()*/
-        }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            readContact()
     }
 
+    private fun readContact() {
+        Toast.makeText(applicationContext, "Permission", Toast.LENGTH_LONG).show()
+    }
 }
